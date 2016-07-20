4ce354a3d17226c360fe261c913c31345749a0ab
Felipe Couto <fel.h2o@gmail.com>
2016-07-20 10:23:44 +0200

    Update check_procs_many.sh

--- check_procs_many.sh
+++ check_procs_many.sh
@@ -1,28 +1,4 @@
-#!/bin/bash
-#
-# Felipe Ferreira
-# 04/07/2016
 
-# fix 06/07/2016 - grep and su showing on ps
-
-SERVICE="$1"
-USER="$2"
-if [ -z $USER ]; then
-        echo "UNKONW - Please set SERVICE and USER arguments"
-        exit 3
-fi
-RESULT=$(ps -U $USER -f 2>&1 | grep -v bash |grep -v grep |grep -v su| grep -c  "$SERVICE")
-#echo $RESULT
-if [ ${RESULT} -eq 0 ]; then
-    echo "CRITICAL - Proc $SERVICE with user $USER is not running"
-    exit 2
-else
-    echo "OK - Proc $SERVICE with user $USER is running"
-    exit 0
-fi
-
-
-[root@accerta plugins]# cat check_procs_many.sh
 #!/bin/bash
 #
 # Felipe Ferreira
