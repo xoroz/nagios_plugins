b6938a6307134b111804b8e0570253cecaf89ea7
Felipe Couto <fel.h2o@gmail.com>
2017-01-12 16:54:11 +0100

    Update check_yum_sec.sh

--- check_yum_sec.sh
+++ check_yum_sec.sh
@@ -5,7 +5,11 @@
 # First lets chceck if yum --security exists and works
 
 #MAKE SURE yum-security is installed
-yum --security version >/dev/null 2>&1 || { echo "I require yum-security but it's not installed.  Aborting." >&2; exit 1; }
+if [[ $(yum -h |grep -c "Include security") = 0 ]]; then
+ echo "UNOKWN - Require yum-security but it's not installed.  Aborting."
+ exit 3
+fi
+
 
 #CHECK FOR ARGS
 if [[ $2 ]]; then
@@ -16,9 +20,13 @@ else
  exit 3
 fi
 
-A=$(yum -C --security check-update |grep " needed for security")
-C=$(echo $A|awk '{ print $1 }')
-
+if [[ $(yum -C --security check-update|grep -c "No packages needed") != 0 ]]; then
+ echo "OK - Zero patches needed|sec=0"
+ exit 0
+else
+ A=$(yum -C --security check-update |grep " needed for security")
+ C=$(echo $A|awk '{ print $1 }')
+fi
 
 if [ "$C" -gt "$CRIT" ]; then
  echo "CRITICAL - $A | sec=$C"
