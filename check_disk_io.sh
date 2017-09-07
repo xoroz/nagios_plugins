d692150d289696c6faaf6a2c5d63a8eeb85a35eb
Felipe Couto <fel.h2o@gmail.com>
2017-09-07 11:46:22 +0200

    Create check_disk_io.sh
    simple bash to check IO wait time from iostsat 
    tested on RedHat 6/7

---
+++ check_disk_io.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Felipe Ferreira 07/2016
+#
+# Local Disk await
+# await =  The average time (in milliseconds) for I/O requests issued to the device to be served. This includes the time spent by the requests in  queue  and the time spent servicing them.
+
+# IMPORTANT: Requires iostat
+
+DISK=$1
+WARN=$2
+CRIT=$3
+
+R=$(iostat -d $DISK -xmt  2 3 |grep $DISK |  awk '{ sum += $10; n++ } END { if (n > 0) print sum / n; }' |cut -c 1-3)
+
+if [ -z $R ]; then
+        echo "UNKOWN - Error to get iostat info for disk $DISK"
+        exit 3
+fi
+
+MSG=" Disk $DISK await time is $R ms | await=$R"
+
+if (( $(bc <<< "$R > $CRIT") )); then
+        echo "CRITICAL - $MSG "
+        exit 2
+elif (( $(bc <<< "$R > $WARN") )); then
+        echo "WARNING - $MSG"
+        exit 1
+fi
+echo "OK - $MSG"
+exit 0
